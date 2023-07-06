1e25e21e1c882e55990f5dddf240e0684646f230
Desmond Loh <desmondjwloh@gmail.com>
2023-07-06 14:01:53 +1000

    Refactored common methods in enemies

--- src/main/java/dungeonmania/entities/enemies/ZombieToast.java
+++ src/main/java/dungeonmania/entities/enemies/ZombieToast.java
@@ -1,19 +1,12 @@
 package dungeonmania.entities.enemies;
 
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-
 import dungeonmania.Game;
-import dungeonmania.entities.collectables.potions.InvincibilityPotion;
-import dungeonmania.map.GameMap;
 import dungeonmania.util.Direction;
 import dungeonmania.util.Position;
 
 public class ZombieToast extends Enemy {
     public static final double DEFAULT_HEALTH = 5.0;
     public static final double DEFAULT_ATTACK = 6.0;
-    private Random randGen = new Random();
 
     public ZombieToast(Position position, double health, double attack) {
         super(position, health, attack);
@@ -21,47 +14,31 @@ public class ZombieToast extends Enemy {
 
     @Override
     public void move(Game game) {
-        Position nextPos;
-        GameMap map = game.getMap();
-        if (map.getPlayer().getEffectivePotion() instanceof InvincibilityPotion) {
-            Position plrDiff = Position.calculatePositionBetween(map.getPlayer().getPosition(), getPosition());
-
-            Position moveX = (plrDiff.getX() >= 0) ? Position.translateBy(getPosition(), Direction.RIGHT)
-                    : Position.translateBy(getPosition(), Direction.LEFT);
-            Position moveY = (plrDiff.getY() >= 0) ? Position.translateBy(getPosition(), Direction.UP)
-                    : Position.translateBy(getPosition(), Direction.DOWN);
-            Position offset = getPosition();
-            if (plrDiff.getY() == 0 && map.canMoveTo(this, moveX))
-                offset = moveX;
-            else if (plrDiff.getX() == 0 && map.canMoveTo(this, moveY))
-                offset = moveY;
-            else if (Math.abs(plrDiff.getX()) >= Math.abs(plrDiff.getY())) {
-                if (map.canMoveTo(this, moveX))
-                    offset = moveX;
-                else if (map.canMoveTo(this, moveY))
-                    offset = moveY;
-                else
-                    offset = getPosition();
-            } else {
-                if (map.canMoveTo(this, moveY))
-                    offset = moveY;
-                else if (map.canMoveTo(this, moveX))
-                    offset = moveX;
-                else
-                    offset = getPosition();
-            }
-            nextPos = offset;
+        Position incrementedPosition;
+        if (playerHasInvincibilityPotion(game)) {
+            incrementedPosition = movePlayer(game);
         } else {
-            List<Position> pos = getPosition().getCardinallyAdjacentPositions();
-            pos = pos.stream().filter(p -> map.canMoveTo(this, p)).collect(Collectors.toList());
-            if (pos.size() == 0) {
-                nextPos = getPosition();
-            } else {
-                nextPos = pos.get(randGen.nextInt(pos.size()));
-            }
+            incrementedPosition = moveRandomly(game);
         }
-        game.getMap().moveTo(this, nextPos);
-
+        game.getMap().moveTo(this, incrementedPosition);
     }
 
+    private Position movePlayer(Game game) {
+        Position playerDifference = Position.calculatePositionBetween(game.getMap().getPlayer().getPosition(),
+                getPosition());
+        Position moveX = (playerDifference.getX() >= 0) ? Position.translateBy(getPosition(), Direction.RIGHT)
+                : Position.translateBy(getPosition(), Direction.LEFT);
+        Position moveY = (playerDifference.getY() >= -0) ? Position.translateBy(getPosition(), Direction.UP)
+                : Position.translateBy(getPosition(), Direction.DOWN);
+
+        if (playerDifference.getY() == 0 && game.getMap().canMoveTo(this, moveX)) {
+            return moveX;
+        } else if (playerDifference.getX() == 0 && game.getMap().canMoveTo(this, moveY)) {
+            return moveY;
+        } else if (Math.abs(playerDifference.getX()) >= Math.abs(playerDifference.getY())) {
+            return game.getMap().canMoveTo(this, moveX) ? moveX : getPosition();
+        } else {
+            return game.getMap().canMoveTo(this, moveY) ? moveY : getPosition();
+        }
+    }
 }
