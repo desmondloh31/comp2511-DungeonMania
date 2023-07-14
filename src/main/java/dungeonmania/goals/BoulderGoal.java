bd30469f1f28a5fc6190f4c9389d6c6adcac88b8
kevin <kevin.zhuang@student.unsw.edu.au>
2023-07-14 23:07:02 +1000

    Implemented startegy pattern to apply open closed principle

---
+++ src/main/java/dungeonmania/goals/BoulderGoal.java
@@ -0,0 +1,14 @@
+package dungeonmania.goals;
+
+import dungeonmania.Game;
+import dungeonmania.entities.Switch;
+
+public class BoulderGoal implements GoalStrategy {
+    public boolean achieved(Game game) {
+        return game.getMap().getEntities(Switch.class).stream().allMatch(s -> s.isActivated());
+    }
+
+    public String toString(Game game) {
+        return ":boulders";
+    }
+}
---
+++ src/main/java/dungeonmania/goals/ExitGoal.java
@@ -0,0 +1,24 @@
+package dungeonmania.goals;
+
+import java.util.List;
+
+import dungeonmania.Game;
+import dungeonmania.entities.Entity;
+import dungeonmania.entities.Exit;
+import dungeonmania.entities.Player;
+import dungeonmania.util.Position;
+
+public class ExitGoal implements GoalStrategy {
+    public boolean achieved(Game game) {
+        Player character = game.getPlayer();
+        Position pos = character.getPosition();
+        List<Exit> es = game.getMap().getEntities(Exit.class);
+        if (es == null || es.size() == 0)
+            return false;
+        return es.stream().map(Entity::getPosition).anyMatch(pos::equals);
+    }
+
+    public String toString(Game game) {
+        return ":exit";
+    }
+}
